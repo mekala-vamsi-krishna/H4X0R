3ef773c9677374755426b52f196e46d4fe9d7cf9
vamsi <vamsikrishnayadav2002@gmail.com>
2022-08-09 09:28:39 +0530

    Initial Commit

---
+++ H4x0R News/H4x0R_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4x0R_NewsApp.swift
+//  H4x0R News
+//
+//  Created by Mekala Vamsi Krishna on 09/08/22.
+//
+
+import SwiftUI
+
+@main
+struct H4x0R_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
